cd8e9ddf9962eaff23e93a6cda11fb15a4d566e1
Nirav <gusainirav1@gmail.com>
2021-08-30 23:39:29 +0530

    new

--- Linked-List/reverseLL.cpp
+++ Linked-List/reverseLL.cpp
@@ -14,3 +14,18 @@ ListNode *reverseList(ListNode *head)
     }
     return prev;
 }
+
+// recursive
+
+ListNode *reverseList(ListNode *head)
+{
+    if (!head || !head->next)
+        return head;
+    ListNode *ans;
+
+    ans = reverseList(head->next);
+
+    head->next->next = head;
+    head->next = NULL;
+    return ans;
+}
