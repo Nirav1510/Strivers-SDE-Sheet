3c94328c689432e8b3450c151a65af272f67dc4e
Nirav <gusainirav1@gmail.com>
2021-09-30 12:39:16 +0530

    easy

---
+++ Array/sort01&2.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    void sort012(int A[], int n)
+    {
+        int i = 0, j = 0, k = n - 1;
+        while (j <= k)
+        {
+            if (A[j] == 0)
+                swap(A[i++], A[j]);
+            if (A[j] == 2)
+                swap(A[j], A[k--]);
+            else
+                j++;
+        }
+    }
+};
+
+int main()
+{
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+
+        Solution ob;
+        ob.sort012(a, n);
+
+        for (int i = 0; i < n; i++)
+        {
+            cout << a[i] << " ";
+        }
+
+        cout << endl;
+    }
+    return 0;
+}
